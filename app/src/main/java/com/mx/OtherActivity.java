de726b6da5a1ebdbbb089dcc2905ccbd27949f76
boobooL <boobooMX@163.com>
2016-04-21 10:17:04 +0800

    修改部分文件

--- app/src/main/java/com/mx/OtherActivity.java
+++ app/src/main/java/com/mx/OtherActivity.java
@@ -1,19 +1,17 @@
 package com.mx;
 
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import rx.Observable;
-import rx.Observer;
-import rx.Subscriber;
+import rx.android.schedulers.AndroidSchedulers;
 import rx.functions.Action1;
+import rx.schedulers.Schedulers;
 
 public class OtherActivity extends AppCompatActivity {
 
@@ -36,7 +34,23 @@ public class OtherActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
         testOne();
+        testTwo();
 
+
+
+    }
+
+    private void testTwo() {
+        Observable.just(1,2,3,4)//测试just的方法
+                .subscribeOn(Schedulers.io())//指定 subscribe()发生在IO线程
+                .observeOn(AndroidSchedulers.mainThread())//指定 Subscri的回调发生在主线程
+                .subscribe(new Action1<Integer>() {
+                    @Override
+                    public void call(Integer integer) {
+                        Log.d("Other",integer+"s");
+                        mTvContent2.setText(integer+"");
+                    }
+                });
 
 
     }
@@ -54,30 +68,30 @@ public class OtherActivity extends AppCompatActivity {
                     }
                 });
 
-        final int drawablesRes=R.mipmap.ic_launcher;
-        Observable.create(new Observable.OnSubscribe<Drawable>() {
-            @Override
-            public void call(Subscriber<? super Drawable> subscriber) {
-                Drawable drawable=getTheme().getDrawable(drawablesRes);
-                subscriber.onNext(drawable);
-                subscriber.onCompleted();
-            }
-        }).subscribe(new Observer<Drawable>() {
-            @Override
-            public void onCompleted() {
-
-            }
-
-            @Override
-            public void onError(Throwable e) {
-
-                Toast.makeText(OtherActivity.this,"error",Toast.LENGTH_SHORT).show();;
-            }
-
-            @Override
-            public void onNext(Drawable drawable) {
-                    mImage1.setImageDrawable(drawable);
-            }
-        });
+//        final int drawablesRes=R.mipmap.ic_launcher;
+//        Observable.create(new Observable.OnSubscribe<Drawable>() {
+//            @Override
+//            public void call(Subscriber<? super Drawable> subscriber) {
+//                Drawable drawable=getTheme().getDrawable(drawablesRes);
+//                subscriber.onNext(drawable);
+//                subscriber.onCompleted();
+//            }
+//        }).subscribe(new Observer<Drawable>() {
+//            @Override
+//            public void onCompleted() {
+//
+//            }
+//
+//            @Override
+//            public void onError(Throwable e) {
+//
+//                Toast.makeText(OtherActivity.this,"error",Toast.LENGTH_SHORT).show();;
+//            }
+//
+//            @Override
+//            public void onNext(Drawable drawable) {
+//                    mImage1.setImageDrawable(drawable);
+//            }
+//        });
     }
 }
